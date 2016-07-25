ae45d1a6a01fb3154db64ce7955f6d07d1491028
gsr <460193296@qq.com>
2016-07-25 15:30:40 +0800

    创建hello

---
+++ javatest/src/Test/Hello.java
@@ -0,0 +1,10 @@
+package Test;
+
+/**
+ * Created by Administrator on 2016/7/25 0025.
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("Hello Git");
+    }
+}
