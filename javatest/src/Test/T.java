93e93d065254a566eabc567fff082e8d3f02b1fb
gsr <460193296@qq.com>
2016-07-25 15:23:12 +0800

    提交到本地库

--- javatest/src/Test/T.java
+++ javatest/src/Test/T.java
@@ -4,7 +4,7 @@ package Test;
  * Created by Administrator on 2016/7/12 0012.
  */
 public class T {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("HEllo dragon");
     }
 }
